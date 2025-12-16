cd4f19b0949ea41116f159bc3012b9fccfad0c27
Myriad-Dreamin <camiyoru@gmail.com>
2025-12-16 16:58:39 +0800

    feat: initialize packages

---
+++ packages/ema-ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{margin: '20px'}}>
+          EverMindAgent.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
