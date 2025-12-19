66020f3f2aac137826660d5d17095538bc7780bd
hiyouga <hiyouga@buaa.edu.cn>
2025-12-20 03:16:43 +0800

    update

--- packages/ema-ui/next.config.ts
+++ packages/ema-ui/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { loadEnvConfig } from "@next/env";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir + "/../../");
+
 
 export default nextConfig;
