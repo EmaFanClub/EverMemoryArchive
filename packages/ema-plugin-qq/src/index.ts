Myriad-Dreamin/EverMindAgent

feat: quick impl

packages/ema-plugin-qq/src/index.ts
@@ -1,11 +1,11 @@
-import type { EmaPluginProvider, Server } from "ema";
-import { NCWebsocket } from "node-napcat-ts";
+import type { AgentEventContent, EmaPluginProvider, Server } from "ema";
+import { NCWebsocket, type GroupMessage } from "node-napcat-ts";
 
 const napcat = new NCWebsocket(
   {
     protocol: "ws",
-    host: "localhost",
-    port: 6099,
+    host: "172.19.0.2",
+    port: 6097,
     accessToken: process.env.NAPCAT_ACCESS_TOKEN,
     // 是否需要在触发 socket.error 时抛出错误, 默认关闭
     throwPromise: true,
@@ -20,26 +20,104 @@ const napcat = new NCWebsocket(
   true,
 );
 
-await napcat.send_group_msg({
-  group_id: 1044916258,
-  message: [
-    {
-      type: "text",
-      data: {
-        text: "全局消息：喵",
-      },
-    },
-  ],
-});
-
 export const Plugin: EmaPluginProvider = class {
   static name = "QQ";
   constructor(private readonly server: Server) {}
-  start(): Promise<void> {
-    console.log("[ema-qq] started", !!this.server.chat);
+  async start(): Promise<void> {
+    const replyPat = process.env.NAPCAT_REPLY_PATTERN;
+    if (!replyPat) {
+      throw new Error("NAPCAT_REPLY_PATTERN is not set");
+    }
 
-    napcat.on("message.group", (message) => {
+    await napcat.connect();
+
+    const actor = await this.server.getActor(1, 1);
+
+    interface GroupMessageTask {
+      message: GroupMessage;
+    }
+
+    let taskId = 0;
+    const tasks: Record<number, GroupMessageTask> = {};
+    const messageCache = new Map<number, string>();
+
+    actor.subscribe((response) => {
+      console.log("[ema-qq] actor response", response);
+      for (const event of response.events) {
+        console.log("[ema-qq] actor event", event);
+        if (event.type === "runFinished") {
+          const runFinishedEvent =
+            event.content as AgentEventContent<"runFinished">;
+          console.log("[ema-qq] actor run finished", runFinishedEvent);
+          if (runFinishedEvent.ok) {
+            const task = tasks[runFinishedEvent.metadata.taskId];
+            if (!task) {
+              console.error(
+                "[ema-qq] task not found",
+                runFinishedEvent.metadata.taskId,
+              );
+              continue;
+            }
+            const message = task.message;
+            message.quick_action(
+              [
+                {
+                  type: "text",
+                  data: {
+                    text: ` ${runFinishedEvent.msg.trim()}`,
+                  },
+                },
+              ],
+              true,
+            );
+          }
+        }
+      }
+    });
+
+    napcat.on("message.group", async (message) => {
+      //  { type: 'reply', data: [Object] },
+      console.log("[ema-qq] group message");
       console.log("[ema-qq] group message", message);
+
+      if (!message.raw_message.includes(replyPat)) {
+        console.log("message ignored");
+        return;
+      }
+      let replyContext = "";
+      const reply = message.message.find((m) => m.type === "reply");
+      if (reply) {
+        const replyId = Number.parseInt(reply?.data.id);
+        if (replyId && !Number.isNaN(replyId)) {
+          const cached = messageCache.get(replyId);
+          if (cached) {
+            replyContext = cached;
+          } else {
+            const msg = await napcat.get_msg({ message_id: replyId });
+            if (msg) {
+              replyContext = msg.raw_message;
+              messageCache.set(replyId, replyContext);
+            }
+          }
+        }
+      }
+      messageCache.set(message.message_id, message.raw_message);
+
+      const id = taskId++;
+      tasks[id] = { message };
+
+      let content = [];
+      if (replyContext) {
+        content.push(`注意：这则消息是在回复：<Reply>`);
+        content.push(replyContext);
+        content.push(`</Reply>`);
+      }
+      content.push(message.raw_message);
+
+      actor.work({
+        metadata: { taskId: id },
+        inputs: [{ kind: "text", content: content.join("\n") }],
+      });
     });
 
     return Promise.resolve();

EmaFanClub/EverMemoryArchive

packages/ema-plugin-qq/src/index.ts
@@ -1,11 +1,11 @@
-import type { EmaPluginProvider, Server } from "ema";
-import { NCWebsocket } from "node-napcat-ts";
+import type { AgentEvent, EmaPluginProvider, Server } from "ema";
+import { NCWebsocket, type GroupMessage } from "node-napcat-ts";
 
 const napcat = new NCWebsocket(
   {
     protocol: "ws",
-    host: "localhost",
-    port: 6099,
+    host: "172.19.0.2",
+    port: 6097,
     accessToken: process.env.NAPCAT_ACCESS_TOKEN,
     // 是否需要在触发 socket.error 时抛出错误, 默认关闭
     throwPromise: true,
@@ -20,26 +20,103 @@ const napcat = new NCWebsocket(
   true,
 );
 
-await napcat.send_group_msg({
-  group_id: 1044916258,
-  message: [
-    {
-      type: "text",
-      data: {
-        text: "全局消息：喵",
-      },
-    },
-  ],
-});
-
 export const Plugin: EmaPluginProvider = class {
   static name = "QQ";
-  constructor(private readonly server: Server) {}
-  start(): Promise<void> {
-    console.log("[ema-qq] started", !!this.server.chat);
+  constructor(private readonly server: Server) { }
+  async start(): Promise<void> {
+    const replyPat = process.env.NAPCAT_REPLY_PATTERN;
+    if (!replyPat) {
+      throw new Error("NAPCAT_REPLY_PATTERN is not set");
+    }
 
-    napcat.on("message.group", (message) => {
+    await napcat.connect();
+
+    const actor = await this.server.getActor(1, 1, 1);
+
+    interface GroupMessageTask {
+      message: GroupMessage;
+    }
+
+    let taskId = 0;
+    const tasks: Record<number, GroupMessageTask> = {};
+    const messageCache = new Map<number, string>();
+
+    actor.events.on('agent', (response) => {
+      console.log("[ema-qq] actor response", response);
+
+      if (response.kind === "runFinished") {
+        const runFinishedEvent = response.content as AgentEvent<"runFinished">;
+        console.log("[ema-qq] actor run finished", runFinishedEvent);
+        if (runFinishedEvent.ok) {
+          const lastMetadata = (runFinishedEvent.metadata instanceof Array) ? ((runFinishedEvent.metadata as any[]).at(-1)) : runFinishedEvent.metadata;
+
+          const task = tasks[lastMetadata.taskId];
+          if (!task) {
+            console.error(
+              "[ema-qq] task not found",
+              lastMetadata.taskId,
+            );
+            return;
+          }
+          const message = task.message;
+          message.quick_action(
+            [
+              {
+                type: "text",
+                data: {
+                  text: ` ${runFinishedEvent.msg.trim()}`,
+                },
+              },
+            ],
+            true,
+          );
+        }
+      }
+    });
+
+    napcat.on("message.group", async (message) => {
+      //  { type: 'reply', data: [Object] },
+      console.log("[ema-qq] group message");
       console.log("[ema-qq] group message", message);
+
+      if (!message.raw_message.includes(replyPat)) {
+        console.log("message ignored");
+        return;
+      }
+      let replyContext = "";
+      const reply = message.message.find((m) => m.type === "reply");
+      if (reply) {
+        const replyId = Number.parseInt(reply?.data.id);
+        if (replyId && !Number.isNaN(replyId)) {
+          const cached = messageCache.get(replyId);
+          if (cached) {
+            replyContext = cached;
+          } else {
+            const msg = await napcat.get_msg({ message_id: replyId });
+            if (msg) {
+              replyContext = msg.raw_message;
+              messageCache.set(replyId, replyContext);
+            }
+          }
+        }
+      }
+      messageCache.set(message.message_id, message.raw_message);
+
+      const id = taskId++;
+      tasks[id] = { message };
+
+      let content = [];
+      if (replyContext) {
+        content.push(`注意：这则消息是在回复：<Reply>`);
+        content.push(replyContext);
+        content.push(`</Reply>`);
+      }
+      content.push(message.raw_message);
+
+      actor.work({
+        metadata: { taskId: id },
+        inputs: [{ type: "text", text: content.join("\n") }],
+      });
     });
 
     return Promise.resolve();